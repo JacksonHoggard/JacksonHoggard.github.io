1cd8d2b3b771838a312d231129ec075190619636
JacksonHoggard <jhoggard0129@gmail.com>
2025-05-12 15:16:01 -0500

    Update Vite configuration to include and exclude specific file patterns

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  include: ['src/**/*.js', 'src/**/*.jsx'],
+  exclude: ['node_modules', 'dist'],
   plugins: [react()],
 })
